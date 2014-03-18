47073ec1abd38b02bcb4b1ddc88a99124c93265d
root <root@gallery.berlin.strato.de>
2014-03-18 17:08:46 +0100

    add settings

--- src/classes/MenuBar.php
+++ src/classes/MenuBar.php
@@ -93,7 +93,7 @@ class MenuBar implements HTMLObject{
 		//echo "<a href='?a=rss'>RSS <img src='./inc/rss.png' height='11px'></a>\n";
 		echo "</div>\n";
 
-		echo "<span>".Settings::_("menubar","powered")." <a href='http://www.photoshow-gallery.com'>PhotoShow</a> - © 2011 Thibaud Rohmer</span>";
+		echo "<span>".Settings::_("menubar","powered")." <a href='http://www.photoshow-gallery.com'>PhotoShow</a> - ©</span>";
 
 		echo "</div>\n";
 	}
